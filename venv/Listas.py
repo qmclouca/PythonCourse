95cda000e47e7c9df3e960d88551325b96a1d7f3
qmclouca <qmclouca@gmail.com>
2021-05-09 12:07:56 -0300

    lists

--- venv/Listas.py
+++ venv/Listas.py
@@ -2,7 +2,7 @@
 Listas em python funcionam como arrays e são dinâmicas
 podendo ser usadas com qualquer tipo de dado
     -Não possuem tamanho fixo
-    -Não possuem tipo de dado fixo mas usa apenas um tipo em cada tempo
+    -Não possuem tipo de dado fixo
     usam [ ]
 """
 #encontrar valor em lista (pode ser qualquer tipo, string, boolean, int, float)
@@ -33,4 +33,19 @@ def listas():
     print(f'Ocorrências do número 5 na lista: {lista1.count(5)}')
     lista1.pop()
     print('Lista após pop: ', *lista1)
-
+    lista1.append([1, 2, 3])
+    print('Lista após append: ', *lista1)
+    lista1.pop()
+    lista1.extend([1, 2, 3])
+    print('Lista após extend: ', *lista1)
+    lista1.insert(3, 'Novo_Valor')
+    print('Lista após inserção na posição 2 do valor (Novo_Valor): ', *lista1)
+    lista3 = lista1 + string_list
+    print('Concatenando listas: ', *lista3)
+    lista1.pop(3)
+    print('Retirando o Novo_valor da lista: ', *lista1)
+    print('Usando slice para modificar lista: ', *lista1[5:2:-1])
+    print('Usando slice para modificar lista: ', *lista1[1:10:2])
+    lista2 = lista1.copy()
+    print('lista2 como cópia da lista 1: ', *lista2)
+    print('Tamanho da lista2: ', len(lista2))
