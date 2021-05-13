1cede46a085b0a0c86fabc1597bdb8d3a74e4bc5
qmclouca <qmclouca@gmail.com>
2021-05-13 17:33:59 -0300

    Estudo de dicionários 2

--- venv/Dictionaries.py
+++ venv/Dictionaries.py
@@ -20,5 +20,27 @@ def dictionaries():
 
     #Acessar via get
     print(paises.get('br'))
-    print(paises.get('ru')) # Não causa erro e por isso é preferida
+    print(paises.get('ru')) # Não causa erro e por isso é preferida no caso de não encontrar o valor para a chave retorna None
 
+    #Verificar a ocorrencia de uma chave ou objeto no dicionário
+    print(f'A chave br está no dicionário? ', 'br' in paises)
+    print(f'O objeto Estados Unidos está no dicionário? ', "Estados Unidos" in paises1)
+    print(f'A chave ru está no dicionário? ', 'ru' in paises)
+
+    #Podem ser utilizados quaisquer tipos de dados (int, float, string, boolean), inclusive listas, tuplas, dicionários como chaves
+    localidades = {
+        (35.6895, 39.6917): 'Escritório de Tókio',
+        (40.7128, 74.0060): 'Escritório de Nova York',
+        (37.7749, 122.4194): 'Escritório de São Paulo'
+    }
+    print(localidades)
+    print(localidades.get((40.7128, 74.0060)))
+    # Adicionar elementos em um dicionário
+    receita = {'jan': 100, 'fev': 120, 'mar': 300}
+    # Forma 1
+    print(receita)
+    receita['abr'] = 350
+    print(receita)
+    # Forma 2
+    receita.update({'mai': 500})
+    print(receita)
